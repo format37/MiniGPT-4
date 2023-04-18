97e51960b0fbe1da1f9201bf76a8ed6ac20a628d
alex <format37@gmail.com>
2023-04-18 14:47:56 +0400

    new file:   transformers_llama_weights_preparation.sh 	new file:   vicuna_weights_preparation.sh

---
+++ transformers_llama_weights_preparation.sh
@@ -0,0 +1,2 @@
+# from https://github.com/huggingface/transformers path
+python3 src/transformers/models/llama/convert_llama_weights_to_hf.py     --input_dir /mnt/sda/share/alex/models/LLaMA --model_size 13B --output_dir /home/alex/projects/MiniGPT-4/models/llama_13b
---
+++ vicuna_weights_preparation.sh
@@ -0,0 +1,1 @@
+python3 -m fastchat.model.apply_delta     --base /home/alex/projects/MiniGPT-4/models/llama_13b     --target models/vicuna-13b     --delta lmsys/vicuna-13b-delta-v1.1
